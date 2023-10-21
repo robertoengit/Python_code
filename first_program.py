3526360799b45d5d22aa1c316108f6fb3f6243d9
robertoengit <root_line@hotmail.com>
2023-10-21 02:10:00 +0000

    commit first

---
+++ first_program.py
@@ -0,0 +1,10 @@
+#this is my first program on python
+
+print('Hello from python')
+title = 'TITLE'
+counter=0
+for x in title:
+    print(x)
+    if x == 'T':
+        counter=counter+1
+print(counter)
